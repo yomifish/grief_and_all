7d9c35e1dda1486b852a8d9fd4cdb41136049f44
Yom Fisseha Bekele <yombekele@Yoms-Air.ht.home>
2025-03-14 02:06:13 -0400

    Quartz sync: Mar 14, 2025, 2:06 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "Grief and all",
+    pageTitleSuffix: "of his friends.",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
